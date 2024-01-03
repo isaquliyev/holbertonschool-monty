4c1bd99ecaaec76f870b9e65a6e0d28984073421
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-03 03:40:20 -0800

    betty style provided for pall.c

--- pall.c
+++ pall.c
@@ -1,10 +1,18 @@
 #include "header.h"
 
+/**
+ * pall - print all values in the stack
+ *
+ * @stack: the top element of stack
+ *
+ * @line_number: line number of .m file
+ */
+
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
-	void(line_number);
 
+	void(line_number);
 	if (*stack)
 		temp = *stack;
 	while (*stack)
