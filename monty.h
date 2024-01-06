93554644f46967b598069702dab6453834fae40c
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-06 08:28:20 -0800

    _mul function prototype declared at header.

--- monty.h
+++ monty.h
@@ -78,6 +78,7 @@ void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
 
 void (*get_function(char *opcodes))(stack_t **stack, unsigned int line_number);
 int _isnumber(char *str);
