68996bf67c82def4a7225fa2a6213f1ea32b6ba7
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-06 12:30:09 -0800

    pchar function implemented.

---
+++ pchar.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * pchar - Prints top of the stack value as char.
+ *
+ * @stack: top of stack
+ * @line_number: line number for error message
+ */
+
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack)
+	{
+		free_glob();
+		dprintf(2, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n >= 0 && (*stack)->n < 256)
+		puts((char *)&(*stack)->n);
+	else
+	{
+		free_glob();
+		dprintf(2, "L%u: can't pchar, stack value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
