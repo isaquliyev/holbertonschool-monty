99b4bee92fe85f2254dc786d95c042e2ba7dea9d
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-06 13:14:03 -0800

    pstr function implemented.

---
+++ pstr.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * pstr - Prints the string starting at the top of the stack.
+ * followed by a new line. (like this comment)
+ *
+ * @stack: top of stack
+ * @line_number: line number for error message
+ */
+
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !(*stack)->n || !isascii((*stack)->n))
+	{
+		putchar('\n');
+		exit(EXIT_SUCCESS);
+	}
+	putchar((*stack)->n);
+	pstr(&(*stack)->next, line_number);
+}
