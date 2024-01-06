95c4a1982c5e177d2d7e57c11c651a143cc710da
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-06 08:10:40 -0800

    div function prototype declared at header.

--- monty.h
+++ monty.h
@@ -77,6 +77,7 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
+void div(stack_t **stack, unsigned int line_number);
 
 void (*get_function(char *opcodes))(stack_t **stack, unsigned int line_number);
 int _isnumber(char *str);
