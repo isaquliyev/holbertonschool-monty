e081cee8d253e0215e9bc66fe4be31b2e416526a
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-07 02:24:10 -0800

    rotr function prototype declared at header.

--- monty.h
+++ monty.h
@@ -83,6 +83,7 @@ void _mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
 
 void (*get_function(char *opcodes))(stack_t **stack, unsigned int line_number);
 int _isnumber(char *str);
