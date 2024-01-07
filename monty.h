f78f9269ab27a615d5224cea3dda2b6b14814767
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-07 00:48:10 -0800

    rotl function prototype declared at header.

--- monty.h
+++ monty.h
@@ -82,6 +82,7 @@ void _mul(stack_t **stack, unsigned int line_number);
 void _mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
 
 void (*get_function(char *opcodes))(stack_t **stack, unsigned int line_number);
 int _isnumber(char *str);
